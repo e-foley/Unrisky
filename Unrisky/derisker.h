c3a63c2d986930b08e905377da2cf332eddd67fb
e-foley <efoley@calpoly.edu>
2019-08-22 22:43:29 -0700

    rm unused libraries

--- Unrisky/derisker.h
+++ Unrisky/derisker.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "json11.hpp"
-//#include "tinyxml2.h"
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -96,72 +95,4 @@ public:
     }
     std::cout << std::endl;
   }
-
-  //static void process(const char* const file_name) {
-  //  std::vector<MarketData> markets;
-  //  tinyxml2::XMLDocument document;
-  //  const tinyxml2::XMLError result = document.LoadFile(file_name);
-
-  //  if (result != tinyxml2::XML_SUCCESS) {
-  //    std::cout << "Could not load file " << file_name << ". Code " << static_cast<int>(result) << "\n";
-  //  }
-
-  //  tinyxml2::XMLHandle document_handle(&document);
-  //  tinyxml2::XMLElement* market_element = document_handle.FirstChildElement("MarketList").FirstChildElement("Markets").FirstChildElement("MarketData").ToElement();
-
-  //  while (market_element != nullptr) {
-  //    MarketData data;
-  //    tinyxml2::XMLElement* id_element = market_element->FirstChildElement("ID");
-  //    if (id_element == nullptr) {
-  //      std::cout << "Could not find ID element in MarketData.\n";
-  //    } else if (id_element->QueryIntText(&data.id) != tinyxml2::XML_SUCCESS) {
-  //      std::cout << "Could not parse ID in MarketData.\n";
-  //    }
-
-  //    tinyxml2::XMLElement* name_element = market_element->FirstChildElement("Name");
-  //    if (name_element == nullptr) {
-  //      std::cout << "Could not find Name element in MarketData.\n";
-  //    } else {
-  //      const char* name_text = name_element->GetText();
-  //      if (name_text == nullptr) {
-  //        std::cout << "Empty Name element in MarketData.\n";
-  //      }
-  //      else {
-  //        data.name = name_text;
-  //      }
-  //    }
-  //    
-  //    // Enumerate all the contracts within, noting the cumulative cost of buying all the nos.
-  //    float no_sum = 0.0f;
-  //    int num_contracts = 0;
-  //    tinyxml2::XMLElement* contract_element = tinyxml2::XMLHandle(market_element).FirstChildElement("Contracts").FirstChildElement("MarketContract").ToElement();
-
-  //    while (contract_element != nullptr) {
-  //      ++num_contracts;
-  //      tinyxml2::XMLElement* best_buy_no_cost_element = contract_element->FirstChildElement("BestBuyNoCost");
-  //      if (best_buy_no_cost_element == nullptr) {
-  //        std::cout << "Could not find BustBuyNoCost within MarketContract\n";
-  //      } else {
-  //        float no_cost = 0.0f;
-  //        if (best_buy_no_cost_element->QueryFloatText(&no_cost) != tinyxml2::XML_SUCCESS) {
-  //          // No text is present when the value is "N/A." For us, this is same as buying at 100 cents.
-  //          no_cost = 1.0f;
-  //        }
-  //        no_sum += no_cost;
-  //      }
-  //      contract_element = contract_element->NextSiblingElement("MarketContract");
-  //    }
-
-  //    // Advantage is what we get (in worst case) less what we pay.
-  //    data.advantage = num_contracts - 1 - no_sum;
-  //    markets.push_back(data);
-
-  //    market_element = market_element->NextSiblingElement("MarketData");
-  //  }
-
-  //  // At this point, we've gone through all the markets.
-  //  for (const MarketData& data : markets) {
-  //    std::cout << data.advantage << " for " << data.id << ": " << data.name.c_str() << "\n";
-  //  }
-  //}
 };
