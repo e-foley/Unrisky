de98809bad98387504563b0c8bc827d85bc8ad09
e-foley <efoley@calpoly.edu>
2019-08-26 00:43:32 -0700

    double -> float risk cast

--- Unrisky/Unrisky.cpp
+++ Unrisky/Unrisky.cpp
@@ -12,7 +12,25 @@ int main(int argc, char* argv[]) {
       Derisker::processJson(argv[1], 0.00f);
       break;
     case 3:
-      Derisker::processJson(argv[1], atof(argv[2]));
+      Derisker::processJson(argv[1], static_cast<float>(atof(argv[2])));
       break;
   }
+
+  // Make a pretend market...
+  /*Market market;
+  market.id = 123;
+  market.name = "My market";
+  market.contracts.push_back({ 1, 0.91f });
+  market.contracts.push_back({ 2, 0.91f });
+  market.contracts.push_back({ 3, 0.88f });
+  market.contracts.push_back({ 4, 0.85f });
+  market.contracts.push_back({ 5, 0.82f });
+  market.contracts.push_back({ 6, 0.84f });
+  market.contracts.push_back({ 7, 0.70f });
+
+  MarketOwnership ownership;
+  std::cout << "hello" << std::endl;
+  Derisker::getIdealMarketOwnership(market, 850.00f, 0.10f, &ownership);
+  ownership.printOwnership();
+  std::cout << ownership.getRisk(0.10f) << std::endl;*/
 }
