6faef22fda2092066f985faae1221eb1f058c41e
Ed Foley <ed.l.foley@gmail.com>
2025-11-02 05:48:46 -0800

    rm other non-gui remnants from runner

--- Unrisky/pull_and_process.bat
+++ Unrisky/pull_and_process.bat
@@ -1,15 +1,3 @@
 @echo off
-:do_the_thing
 python pull_new_json.py
 ..\x64\Release\Unrisky.exe
-
-set resp=""
-set /p resp="Check again? (y/N) "
-if "%resp%" == "y" (
-  echo.
-  goto :do_the_thing
-)
-if "%resp%" == "Y" (
-  echo.
-  goto :do_the_thing
-)
